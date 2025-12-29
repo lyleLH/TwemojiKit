779e7541530cfff7c9d5533021a8ba277637a1c0
Hao Liu <v2top1@163.com>
2025-12-29 13:54:19 -0800

    Add UIImage+Twemoji extension and update dependencies

--- Package.swift
+++ Package.swift
@@ -13,11 +13,19 @@ let package = Package(
             name: "TwemojiKit",
             targets: ["TwemojiKit"])
     ],
+    dependencies: [
+        .package(url: "https://github.com/SVGKit/SVGKit.git", from: "3.0.0"),
+        .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.18.10")
+    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "TwemojiKit",
+            dependencies: [
+                .product(name: "SVGKit", package: "SVGKit"),
+                .product(name: "SDWebImage", package: "SDWebImage")
+            ],
             path: "Sources",
             resources: [.copy("./Core/twemoji.min.js")]
         )
